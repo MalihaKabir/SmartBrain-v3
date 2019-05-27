036282779a06f8511412e49977d1c4dc1041ff22
MalihaKabir <malihakabir01@gmail.com>
2019-05-27 23:22:51 +0600

    transferring clarifai API key to the backend to secure it

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
 import Rank from '../components/Rank/Rank';
@@ -9,10 +8,6 @@ import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 import SignIn from '../components/SignIn/SignIn';
 import Register from '../components/Register/Register';
 import './App.css';
-
-const app = new Clarifai.App({
-	apiKey: '036f526ad4d4471d87379a83017d3cbf'
-});
 
 const particlesOptions = {
 	particles: {
@@ -83,8 +78,14 @@ class App extends Component {
 
 	onPictureSubmit = () => {
 		this.setState({ imgURL: this.state.input });
-		app.models
-			.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+		fetch('http://localhost:3001/imageurl', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				input: this.state.input
+			})
+		})
+			.then((response) => response.json())
 			.then((response) => {
 				if (response) {
 					fetch('http://localhost:3001/image', {
